81b1cd4e433e537780ff84e5c7c5ed8dc2f83750
jwaldrip <jason@waldrip.net>
2016-12-14 11:16:59 -0700

    WIP

--- src/psykube.cr
+++ src/psykube.cr
@@ -2,5 +2,7 @@ require "./psykube/manifest"
 
 manifest = Psykube::Manifest.from_yaml(File.read(".psykube.yaml"))
 kube_cm = manifest.kuberenetes_cluster_config_map("production")
+kube_svc = manifest.kuberenetes_cluster_service
 
 puts kube_cm.to_yaml
+puts kube_svc.to_yaml
--- src/psykube/kubernetes/service.cr
+++ src/psykube/kubernetes/service.cr
@@ -1,53 +1,81 @@
 require "yaml"
 require "./shared/metadata"
 
-class Psykube::Kuberenetes::Service
+class Psykube::Kubernetes::Service
   YAML.mapping(
     kind: String,
     apiVersion: String,
     metadata: {type: Psykube::Kubernetes::Shared::Metadata},
-    spec: {type:Spec}  #nillable?
+    spec: {type: Spec},
+    status: {type: Status, nilable: true, setter: false}
   )
+
+  def initialize
+    @kind = "Service"
+    @apiVersion = "v1"
+    @metadata = Psykube::Kubernetes::Shared::Metadata.new
+    @spec = Spec.new
+  end
+
+  def initialize(name : String, ports : Hash(String, UInt16))
+    initialize
+    metadata.name = name
+    spec.selector["app"] = name
+    ports.each do |name, port|
+      spec.ports.push(Spec::Port.new(name, port))
+    end
+  end
 
   class Spec
     YAML.mapping(
-      ports: { type: Array(Port), nilable: true, setter: false},
-      selector: { type: Hash(String,String), default: {} of String => String }, # --> ?
-      clusterIP: { type: String, nilable: true}, #nillable?
-      type: { type: String, nilable: false}, #nillable?
-      externalIPs: { type: Array(String),nilable: true}, #nillable?
-      deprecatedPublicIPs: { type: Array(String), nilable: true}, #nillable?
-      sessionAffinity: { type: String, nilable: true}, #nillable?
-      loadBalancerIP: { type: String, nilable: true}, #nillable
-      loadBalancerSourceRanges: { type: Array(String), nilable: true},  #nillable?
-      externalName: { type: String, nilable: true}, #nillable?
+      ports: {type: Array(Port), setter: false},
+      selector: {type: Hash(String, String), default: {} of String => String},
+      clusterIP: {type: String, nilable: true, setter: false},
+      type: {type: String, nilable: true},
+      externalIPs: {type: Array(String), nilable: true, setter: false},
+      deprecatedPublicIPs: {type: Array(String), nilable: true, setter: false},
+      sessionAffinity: {type: String, nilable: true},
+      loadBalancerIP: {type: String, nilable: true, setter: false},
+      loadBalancerSourceRanges: {type: Array(String), nilable: true},
+      externalName: {type: String, nilable: true},
     )
+
+    def initialize
+      @ports = [] of Port
+      @selector = {} of String => String
+    end
 
     class Port
       YAML.mapping(
-        name: String,
-        protocol: String,
-        port: { type:Uint16},
-        targetPort: String,
-        nodePort: { type: Uint16}
+        name: {type: String},
+        protocol: {type: String},
+        port: {type: UInt16},
+        targetPort: {type: UInt16, nilable: true},
+        nodePort: {type: UInt16, nilable: true}
       )
+
+      def initialize(name : String, port_number : UInt16)
+        @name = name
+        @port = port_number
+        @protocol = "TCP"
+      end
     end
   end
 
   class Status
     YAML.mapping(
-      loadBalancer: { type:LoadBalancer, nillable: true} #nillable?
+      loadBalancer: {type: LoadBalancer, setter: false}
     )
 
     class LoadBalancer
       YAML.mapping(
-        ingress: { type: Array(Ingress), nilable: true} # nillable?
+        ingress: {type: Array(Ingress), setter: false}
       )
 
       class Ingress
         YAML.mapping(
-          ip: String,
-          hostname: String
+          ip: {type: String, setter: false},
+          hostname: {type: String, setter: false}
         )
       end
     end
