3e27ddb5b803ead2d79c018fd021f32f19f54199
Jason Waldrip <jason@waldrip.net>
2016-12-15 11:29:01 -0700

    List (#2)
    * rename
    
    * allow name to be nil
    
    * update namespace
    
    * add list

--- src/psykube.cr
+++ src/psykube.cr
@@ -1,8 +1,12 @@
 require "./psykube/manifest"
+require "./psykube/kubernetes/list"
 
 manifest = Psykube::Manifest.from_yaml(File.read(".psykube.yaml"))
 kube_cm = manifest.kuberenetes_cluster_config_map("production")
 kube_svc = manifest.kuberenetes_cluster_service
+kube_list = Psykube::Kubernetes::List.new([
+  kube_cm,
+  kube_svc
+])
 
-puts kube_cm.to_yaml
-puts kube_svc.to_yaml
+puts kube_list.to_yaml
