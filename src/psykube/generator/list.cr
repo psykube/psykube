c20fbbcfa02367709fed5d6cd12ed26d62ca6fe6
CT Deploy <engineering@commercialtribe.com>
2016-12-20 09:54:38 -0700

    WIP

--- src/psykube/generator/list.cr
+++ src/psykube/generator/list.cr
@@ -8,10 +8,10 @@ class Psykube::Generator
     private def generate_list
       Psykube::Kubernetes::List.new([
         config_map,
-        # secret,
+        secret,
         # deployment,
         service,
-        ingress
+        ingress,
       ])
     end
   end
