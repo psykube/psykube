64e7956043b1270415401cb1fb1eb040d5e6f7bd
Jason Waldrip <jason@waldrip.net>
2016-12-28 17:41:07 -0700

    move to examples and reference

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM ruby
-RUN gem install rack
-ARG NPM_TOKEN
-ENV PORT 8080
-CMD ruby -r rack -e "Rack::Server.start(Host: '0.0.0.0', Port: $PORT, app: ->(env){ [200, {'Content-Type' => 'text/html'}, ['hello world']] })"
---
+++ examples/simple/server.cr
@@ -0,0 +1,9 @@
+require "http/server"
+
+server = HTTP::Server.new(ENV["PORT"]) do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello world, got #{context.request.path}!"
+end
+
+puts "Listening on http://127.0.0.1:8080"
+server.listen
