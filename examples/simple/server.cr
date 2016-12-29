a66695a081de5307be808bf80f0426a94d8bf3bd
Jason Waldrip <jason@waldrip.net>
2016-12-29 08:30:53 -0700

    fix server

--- examples/simple/server.cr
+++ examples/simple/server.cr
@@ -1,9 +1,12 @@
 require "http/server"
 
-server = HTTP::Server.new(ENV["PORT"]) do |context|
+bind_addr = "0.0.0.0"
+port = (ENV["PORT"]? || "8080").to_i
+
+server = HTTP::Server.new(bind_addr, port) do |context|
   context.response.content_type = "text/plain"
   context.response.print "Hello world, got #{context.request.path}!"
 end
 
-puts "Listening on http://127.0.0.1:8080"
+puts "Listening on http://#{bind_addr}:#{port}"
 server.listen
