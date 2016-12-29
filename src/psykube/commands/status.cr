38a3da3159a727b881a3e1a0d6a78c063d15ae79
Jason Waldrip <jason@waldrip.net>
2016-12-29 11:46:36 -0700

    fix status

--- src/psykube/commands/status.cr
+++ src/psykube/commands/status.cr
@@ -4,13 +4,11 @@ require "./push"
 
 module Psykube::Commands
   Status = Commander::Command.new do |cmd|
-    cmd.use = "status <cluster>"
+    cmd.use = "status"
     cmd.short = "Status of the pods belonging to this app."
     cmd.long = cmd.short
     cmd.flags.add Flags::Namespace
     cmd.flags.add Flags::File
-    cmd.flags.add Flags::Image
-    cmd.flags.add Flags::Push
     cmd.run do |options, arguments|
       namespace = options.string["namespace"]
       selector_labels = Helpers.build_selector_labels(cmd, options)
