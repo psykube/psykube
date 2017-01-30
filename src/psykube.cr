a9524adf9d09c4bc4e5273d6173044b8fddf5a95
Jason Waldrip <jason@waldrip.net>
2017-01-30 10:17:21 -0700

    let panic handle it

--- src/psykube.cr
+++ src/psykube.cr
@@ -2,9 +2,4 @@ require "tempfile"
 require "colorize"
 require "./psykube/cli"
 
-begin
-  Psykube::CLI.run
-rescue e
-  STDERR.puts "Error: #{e.message}".colorize(:red)
-  exit(2)
-end
+Psykube::CLI.run
