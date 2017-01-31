5cae3dc38ddf1592053f577aba4567f035ac9080
Jason Waldrip <jason@waldrip.net>
2017-01-31 11:05:07 -0700

    fix conditions

--- src/psykube/kubernetes/deployment/status.cr
+++ src/psykube/kubernetes/deployment/status.cr
@@ -2,6 +2,7 @@ require "../concerns/mapping"
 
 class Psykube::Kubernetes::Deployment::Status
   Kubernetes.mapping({
+    conditions:           {type: Array(Condition), setter: false, nilable: true},
     observed_generation:  {type: UInt32, setter: false, nilable: true},
     replicas:             {type: UInt32, setter: false, nilable: true},
     updated_replicas:     {type: UInt32, setter: false, nilable: true},
@@ -9,3 +10,5 @@ class Psykube::Kubernetes::Deployment::Status
     unavailable_replicas: {type: UInt32, setter: false, nilable: true},
   })
 end
+
+require "./status/*"
