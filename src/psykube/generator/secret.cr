c1635536d275704465b7262828f22f442af169a4
Jason Waldrip <jason@waldrip.net>
2017-02-02 11:01:40 -0700

    secrets needs to be base64 encoded

--- src/psykube/generator/secret.cr
+++ src/psykube/generator/secret.cr
@@ -4,14 +4,20 @@ class Psykube::Generator
   class Secret < Generator
     protected def result
       unless combined_secrets.empty?
-        Kubernetes::Secret.new(manifest.name, combined_secrets).tap do |secret|
+        Kubernetes::Secret.new(manifest.name, encoded_secrets).tap do |secret|
           secret.metadata.namespace = namespace
         end
       end
     end
 
+    private def encoded_secrets
+      combined_secrets.each_with_object({} of String => String) do |(k, v), hash|
+        hash[k] = Base64.encode(v)
+      end
+    end
+
     private def combined_secrets
-      manifest_secrets.merge cluster_secrets
+      manifest_secrets.merge(cluster_secrets)
     end
 
     private def manifest_secrets
