4ec70dec82f0191de88beee6a3a1b41d893d610a
jwaldrip <jason@waldrip.net>
2017-02-14 11:06:14 -0700

    clean rules for job

--- src/psykube/kubernetes/job.cr
+++ src/psykube/kubernetes/job.cr
@@ -6,6 +6,16 @@ class Psykube::Kubernetes::Job
     spec:   Spec?,
     status: {type: Status, nilable: true, clean: true},
   })
+
+  def clean!
+    if labels = spec.try(&.template.metadata.labels)
+      labels.delete("controller-uid")
+    end
+    if match_labels = spec.try(&.selector.match_labels)
+      match_labels.delete("controller-uid")
+    end
+    super
+  end
 end
 
 require "./job/*"
