811d02329762dfa71b824d78adb9b12e6a7e2031
jwaldrip <jason@waldrip.net>
2017-02-16 11:16:55 -0700

    dont clean headless services

--- src/psykube/kubernetes/service/spec.cr
+++ src/psykube/kubernetes/service/spec.cr
@@ -4,7 +4,7 @@ class Psykube::Kubernetes::Service::Spec
   Kubernetes.mapping(
     ports: {type: Array(Psykube::Kubernetes::Service::Spec::Port), setter: false, default: [] of Port},
     selector: {type: Hash(String, String), default: {} of String => String},
-    cluster_ip: {type: String, nilable: true, setter: false, key: "clusterIP", clean: true},
+    cluster_ip: {type: String, nilable: true, setter: false, key: "clusterIP"},
     type: String?,
     external_ips: {type: Array(String), nilable: true, setter: false, key: "externalIPs"},
     deprecated_public_ips: {type: Array(String), nilable: true, setter: false, key: "deprecatedPublicIPs"},
@@ -13,6 +13,13 @@ class Psykube::Kubernetes::Service::Spec
     load_balancer_source_ranges: Array(String)?,
     external_name: Array(String)?,
   )
+
+  def clean!
+    super
+    if @cluster_ip != "None"
+      @cluster_ip = nil
+    end
+  end
 end
 
 require "./spec/*"
