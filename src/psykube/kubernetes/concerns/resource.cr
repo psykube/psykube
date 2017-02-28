275a282b3bdef2d25cde355190244d9b80757482
jwaldrip <jason@waldrip.net>
2017-02-28 15:04:59 -0700

    add annotations

--- src/psykube/kubernetes/concerns/resource.cr
+++ src/psykube/kubernetes/concerns/resource.cr
@@ -2,6 +2,8 @@ require "./mapping"
 require "../shared/metadata"
 
 module Psykube::Kubernetes::Resource
+  private TIMESTAMP = Time::Format::ISO_8601_DATE_TIME.format(Time.utc_now)
+
   macro definition(api_version, kind, properties)
     # Add resource props
     {% properties[:api_version] = {type: String, default: api_version} %}
@@ -12,6 +14,9 @@ module Psykube::Kubernetes::Resource
       initialize
       @metadata = Shared::Metadata.new(name)
       (@metadata.labels ||= {} of String => String)["psykube"] = "true"
+      (@metadata.annotations ||= {} of String => String)["psykube.io/whodunit"] = `whoami`.strip
+      (@metadata.annotations ||= {} of String => String)["psykube.io/cause"] = ([PROGRAM_NAME] + ARGV.to_a).join(" ")
+      (@metadata.annotations ||= {} of String => String)["psykube.io/last-applied-at"] = TIMESTAMP
     end
 
     def name
