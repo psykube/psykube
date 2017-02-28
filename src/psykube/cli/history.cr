3af64b39853432308792fa26eb29a2e398de7f4c
jwaldrip <jason@waldrip.net>
2017-02-28 15:07:20 -0700

    update history

--- src/psykube/cli/history.cr
+++ src/psykube/cli/history.cr
@@ -5,7 +5,7 @@ class Psykube::Commands::History < Admiral::Command
   include Kubectl
   include KubectlAll
 
-  define_help description: "Apply the kubernetes manifests."
+  define_help description: "Get the rollout history of a deployment."
 
   def run
     kubectl_exec("rollout", ["history", "deployment/#{generator.manifest.name}"])
