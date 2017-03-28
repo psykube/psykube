97cba25d5b3f4b801c62c080650a78de7c97d60d
jwaldrip <jason@waldrip.net>
2017-03-28 15:53:24 -0600

    fix unknown location errors

--- src/psykube/manifest/parse_exception.cr
+++ src/psykube/manifest/parse_exception.cr
@@ -4,8 +4,8 @@ class Psykube::Manifest::ParseException < Exception
   def initialize(template : String, error : YAML::ParseException)
     lines = template.lines
     col = error.column_number - 1
-    carat = (" " * col) + "^ " + error.message.to_s
-    lines.insert(error.line_number, carat.colorize(:red).to_s)
+    err = col > 0 ? (" " * col) + "^ " + error.message.to_s : error.message.to_s
+    lines.insert(error.line_number, err.colorize(:red).to_s)
     lines.unshift "Error: Could not parse manifest\n".colorize(:red).to_s
     initialize lines.join("\n")
   end
