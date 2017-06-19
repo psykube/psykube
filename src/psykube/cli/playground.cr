ed9180c2b4d26aed4305888766b5d4fc4ad27674
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2017-06-19 12:51:46 -0600

    Updated max surge and unavailble rollout fields to allow strings, def… (#38)
    * Updated max surge and unavailble rollout fields to allow strings, defaulted value to k8s doc value of 25%.
    
    * introduce percentage
    
    * use a recent node
    
    * add qos class
    
    * allow nilable

--- src/psykube/cli/playground.cr
+++ src/psykube/cli/playground.cr
@@ -18,7 +18,7 @@ class Psykube::Commands::Playground < Admiral::Command
         gen = Generator::List.new(body)
         gen.to_json(context.response)
       end
-    rescue e : Psykube::ParseException | Crustache::ParseError | Generator::ValidationError
+    rescue e : Psykube::ParseException | Crustache::ParseError | Generator::ValidationError | ArgumentError
       context.response.status_code = 422
       context.response << e.message
     end
