a6ff023663309bfb6464862a57cc5211f7b8e000
jwaldrip <jason@waldrip.net>
2017-06-19 09:54:24 -0600

    command aliases

--- src/psykube/cli.cr
+++ src/psykube/cli.cr
@@ -17,22 +17,22 @@ class Psykube::CLI < Admiral::Command
   define_version VERSION
   define_help
 
-  register_sub_command apply, Commands::Apply
+  register_sub_command apply, Commands::Apply, short: a
   register_sub_command "copy-namespace", Commands::CopyNamespace
   register_sub_command "copy-resource", Commands::CopyResource
-  register_sub_command generate, Commands::Generate
-  register_sub_command delete, Commands::Delete
-  register_sub_command exec, Commands::Exec
-  register_sub_command "port-forward", Commands::PortForward
+  register_sub_command generate, Commands::Generate, short: g
+  register_sub_command delete, Commands::Delete, short: d
+  register_sub_command exec, Commands::Exec, short: e
+  register_sub_command "port-forward", Commands::PortForward, short: pf
   {% if env("EXCLUDE_DOCKER") != "true" %}
-  register_sub_command push, Commands::Push
+  register_sub_command push, Commands::Push, short: p
   {% end %}
-  register_sub_command status, Commands::Status
-  register_sub_command logs, Commands::Logs
+  register_sub_command status, Commands::Status, short: s
+  register_sub_command logs, Commands::Logs, short: l
   register_sub_command init, Commands::Init
   register_sub_command history, Commands::History
   register_sub_command rollback, Commands::Rollback
   register_sub_command scale, Commands::Scale
   register_sub_command playground, Commands::Playground
-  register_sub_command validate, Commands::Validate
+  register_sub_command validate, Commands::Validate, short: v
 end
