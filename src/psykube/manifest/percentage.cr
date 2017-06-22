f08a371626ca6c1f09c6131ee3c5e26a95acdec4
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2017-06-22 15:11:05 -0600

    Fix max_surge and max_available (#42)
    * Update refernce yaml.
    
    * Fixed spacing, better error logging.
    
    * fix yaml and json parsing
    
    * Removed version from gcloud component update.
    
    * Update before script install steps.
    
    * Disable prompts, cache gcp dir.

--- src/psykube/manifest/percentage.cr
+++ src/psykube/manifest/percentage.cr
@@ -7,19 +7,23 @@ class Psykube::Manifest::Percentage
 
   def initialize(pull : YAML::PullParser)
     initialize pull.read_scalar
+  rescue ex
+    raise YAML::ParseException.new(ex.message.not_nil!, 0, 0)
   end
 
   def initialize(pull : JSON::PullParser)
     initialize pull.read_string
+  rescue ex
+    raise JSON::ParseException.new(ex.message.not_nil!, 0, 0)
   end
 
-  def initialize(value : String)
-    raise InvalidError.new("is not a valid percentage") unless value.ends_with?('%')
-		initialize value.chomp('%').to_u32
+  def initialize(@value : String)
+    raise InvalidError.new("#{value} is not a valid percentage") unless value.ends_with?('%')
+    initialize value.chomp('%').to_u32
   end
 
   def initialize(value : Int32)
-		initialize value.to_u32
+    initialize value.to_u32
   end
 
   def initialize(@value : UInt32)
@@ -30,11 +34,11 @@ class Psykube::Manifest::Percentage
   end
 
   def to_json(io)
-		to_s.to_json(io)
+    to_s.to_json(io)
   end
 
   def to_yaml(io)
-		to_s.to_yaml(io)
+    to_s.to_yaml(io)
   end
 
 end
