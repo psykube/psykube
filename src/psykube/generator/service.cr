4fa466c4b17e0e52c8a17a3f6cb3d3468631a875
jwaldrip <jason@waldrip.net>
2017-09-09 22:35:22 -0600

    fix missing selector port on service

--- src/psykube/generator/service.cr
+++ src/psykube/generator/service.cr
@@ -1,10 +1,13 @@
 abstract class Psykube::Generator
   class Service < Generator
+    include Concerns::PodHelper
+
     protected def result
       if (service = manifest.service)
         Kubernetes::Api::V1::Service.new(
-          metadata: generate_metadata(annotations: [service.annotations]),
+          metadata: generate_metadata(labels: [{ "service" => name }], annotations: [service.annotations]),
           spec: Kubernetes::Api::V1::ServiceSpec.new(
+            selector: generate_selector.match_labels,
             type: service.type,
             cluster_ip: service.cluster_ip,
             load_balancer_ip: service.load_balancer_ip,
@@ -21,7 +24,8 @@ abstract class Psykube::Generator
       manifest.ports.map do |name, port|
         Kubernetes::Api::V1::ServicePort.new(
           name: name,
-          port: port
+          port: port,
+          protocol: "TCP"
         )
       end
     end
