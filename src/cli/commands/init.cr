59528dcf645734aea0fb57e6f65fc02ec397b89e
jwaldrip <jason@waldrip.net>
2018-02-28 13:25:35 -0700

    move manifest init into the manifest

--- src/cli/commands/init.cr
+++ src/cli/commands/init.cr
@@ -1,20 +1,13 @@
 require "admiral"
 require "./concerns/*"
 
-def current_docker_user
-  {% if env("EXCLUDE_DOCKER") != "true" %}
-  `#{Psykube::CLI::Commands::Docker.bin} info`.lines.find(&.=~ /^Username/).try(&.split(":")[1]?).to_s.strip
-  {% else %}
-  ""
-  {% end %}
-end
-
 class Psykube::CLI::Commands::Init < Admiral::Command
   include PsykubeFileFlag
 
   define_help description: "Generate a .psykube.yml in the current directory."
 
   define_flag overwrite : Bool, "Overwrite the file if it exists", short: 'o'
+  define_flag type, "Set the type of pyskube manifest.", short: 't', default: "Deployment"
   define_flag name, "Set the name of the application.", short: 'N'
   define_flag namespace, "Set the namespace to deploy into.", short: 'n'
   define_flag registry_host, "The hostname for the registry.", short: 'H'
@@ -22,7 +15,7 @@ class Psykube::CLI::Commands::Init < Admiral::Command
   define_flag ports : Array(String), "Set a port. (can be in the format of --port 1234 or --port http=1234).", long: "port", short: "p", default: [] of String
   define_flag env : Array(String), short: "e", default: [] of String
   define_flag hosts : Array(String), "Set a host for ingress.", long: "host", default: [] of String
-  define_flag tls : Bool, "Enable TLS for ingress.", short: 't'
+  define_flag tls : Bool, "Enable TLS for ingress."
   define_flag image, "Set the image, this takes precedence over --registry-host and --registry-user.", short: 'i'
   define_flag cpu_request, "Set the requested cpu resources."
   define_flag memory_request, "Set the requested memory resources."
@@ -39,39 +32,7 @@ class Psykube::CLI::Commands::Init < Admiral::Command
     if !File.exists?(flags.file) || overwrite?
       puts "Writing #{flags.file}...".colorize(:cyan)
       File.open(flags.file, "w+") do |file|
-        manifest = Psykube::Manifest.new(flags.name || File.basename(Dir.current))
-        if ingress = manifest.ingress
-          ingress.annotations = nil
-          ingress.hosts = nil
-        end
-        if flags.image
-          manifest.image = flags.image
-        else
-          manifest.registry_host = flags.registry_host
-          manifest.registry_user = flags.registry_user || current_docker_user
-        end
-        manifest.resources = Manifest::Resources.from_flags(
-          flags.cpu_request,
-          flags.memory_request,
-          flags.cpu_limit,
-          flags.memory_limit
-        )
-        manifest.namespace = flags.namespace
-        manifest.ports = Hash(String, Int32).new.tap do |hash|
-          flags.ports.each_with_index do |spec, index|
-            parts = spec.split("=", 2).reverse
-            port = parts[0].to_i? || raise "Invalid port format."
-            name = parts[1]? || (index == 0 ? "default" : "port_#{index}")
-            hash[name] = port
-          end
-        end unless flags.ports.empty?
-        manifest.env = flags.env.map(&.split('=')).each_with_object(Hash(String, Manifest::Env | String).new) do |(k, v), memo|
-          memo[k] = v
-        end unless flags.env.empty?
-        manifest.clusters = {
-          "default" => Manifest::Cluster.new(context: `#{Kubectl.bin} config current-context`.strip),
-        }
-        manifest.ingress = Manifest::Ingress.new(hosts: flags.hosts, tls: flags.tls) unless flags.hosts.empty?
+        manifest = Psykube::V1::Manifest.new(self)
         string = manifest.to_yaml
         file.write string.lines[1..-1].join("\n").to_slice
       end
--- src/psykube.cr
+++ src/psykube.cr
@@ -14,6 +14,18 @@ module Psykube
     "psykube.io/cause"           => ([PROGRAM_NAME] + ARGV.to_a).join(" "),
     "psykube.io/last-applied-at" => Time::Format::ISO_8601_DATE_TIME.format(Time.utc_now),
   }
+
+  def self.current_docker_user
+    {% if env("EXCLUDE_DOCKER") != "true" %}
+    `#{Psykube::CLI::Commands::Docker.bin} info`.lines.find(&.=~ /^Username/).try(&.split(":")[1]?).to_s.strip
+    {% else %}
+    ""
+    {% end %}
+  end
+
+  def self.current_kubectl_context
+    `#{Psykube::CLI::Commands::Kubectl.bin} config current-context`.strip
+  end
 end
 
 require "./psykube/*"
