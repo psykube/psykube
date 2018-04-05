9fd6a6eec6e13ae2e5d5dca6aa54a50ccd9ae012
jwaldrip <jason@waldrip.net>
2018-04-05 15:29:58 -0600

    fix playground errors

--- src/playground/generate_controller.cr
+++ src/playground/generate_controller.cr
@@ -6,7 +6,7 @@ struct Psykube::Playground::GenerateController
       gen = Actor.new(body).generate
       gen.to_json(context.response)
     end
-  rescue e : Psykube::ParseException | Crustache::ParseError | Generator::ValidationError | ArgumentError
+  rescue e : Psykube::Error | YAML::ParseException
     context.response.status_code = 422
     context.response << e.message
   end
