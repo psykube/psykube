486fe3846ad827f69df00e08e8c8ac198a0c659a
Jason Waldrip <jason@waldrip.net>
2018-04-12 17:39:22 -0600

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -10,7 +10,6 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 
 # Install Psykube
 PSYKUBE_RELEASES_URL=https://api.github.com/repos/psykube/psykube/releases/tags/${PSYKUBE_VERSION}
-PSYKUBE_RELEASE_RESULTS=`curl -fsSL -H "Authorization: token ${GITHUB_API_TOKEN}" ${PSYKUBE_RELEASES_URL} || curl -fsSL ${PSYKUBE_RELEASES_URL}`
-# PSYKUBE_RELEASE_RESULTS=`curl -fsSL ${PSYKUBE_RELEASES_URL}`
+PSYKUBE_RELEASE_RESULTS=`curl -fsSL -H "Authorization: token ${GITHUB_API_TOKEN}" ${PSYKUBE_RELEASES_URL}`
 PSYKUBE_DOWNLOAD_URL=`echo $PSYKUBE_RELEASE_RESULTS | jq -r '.assets[] | select(.name | contains("linux")).browser_download_url'`
 curl -fsSL ${PSYKUBE_DOWNLOAD_URL} | sudo tar -xzC /usr/local/bin
